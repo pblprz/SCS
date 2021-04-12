e80140d2d6d1909c63c68a4723f6a7ea0e014731
plbprz <pablopj.97@gmail.com>
2021-04-12 14:08:32 +0200

    Second version with key rotation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# SCS
-Lab 2 de SPEA
+# SPEA
+
+## Secure Cloud Storage 
+La carpeta templates contiene los ficheros .html que se ejecutarán en el navegador del cliente.
+El fichero index.py se ejecutará en el servidor y contiene toda la complejidad de la aplicación.
+
+### Autores
+Pablo Pérez y Jesús Tovar
--- index.py
+++ index.py
@@ -8,30 +8,40 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 import boto3
 import base64
-from io import StringIO, BytesIO
+from io import BytesIO
 from dotenv import load_dotenv
 
-load_dotenv()
+app = Flask(__name__)
 
+# AWS keys from .env
+load_dotenv()
 AWS_ACCESS_KEY_ID=os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY_ID=os.getenv('AWS_SECRET_ACCESS_KEY_ID')
 
-app = Flask(__name__)
-path = './unsecure/'
+# Variables
+path = './files/'
 user_password = {}
 user_mode = {}
-user_key = {}
+kms_client = boto3.client("kms", region_name='eu-central-1', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY_ID)
+NUM_BYTES_FOR_LEN = 4
 
-#kms_client = boto3.client("kms", region_name='eu-central-1', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY_ID)
+# Create directory for files if it doesn't exist
+try:
+    os.mkdir(path)
+except:
+    pass
 
+# Create AWS key with key rotation
 def create_cmk(description):
     """Creates a KMS Customer Master Key
 
     Description is used to differentiate between CMKs.
     """
 
+    # Create key
     response = kms_client.create_key(Description=description)
     
+    # Enable key rotation
     response2 = kms_client.enable_key_rotation(KeyId=response["KeyMetadata"]["KeyId"])
 
     # Return the key ID and ARN
@@ -47,10 +57,10 @@ def retrieve_cmk(description):
     for cmk in response["Keys"]:
         key_info = kms_client.describe_key(KeyId=cmk["KeyArn"])
         if key_info["KeyMetadata"]["Description"] == description:
-            return cmk["KeyId"]#, cmk["KeyArn"]
+            return cmk["KeyId"] #, cmk["KeyArn"]
 
     # No matching CMK found
-    return None#, None
+    return None #, None
     
 def create_data_key(cmk_id, key_spec="AES_256"):
     """Generate a data key to use when encrypting and decrypting data"""
@@ -59,9 +69,8 @@ def create_data_key(cmk_id, key_spec="AES_256"):
     response = kms_client.generate_data_key(KeyId=cmk_id, KeySpec=key_spec)
 
     # Return the encrypted and plaintext data key
-    #return response["CiphertextBlob"], base64.b64encode(response["Plaintext"])
-    return base64.b64encode(response["Plaintext"])
-    
+    return response["CiphertextBlob"], base64.b64encode(response["Plaintext"])
+
 def decrypt_data_key(data_key_encrypted):
     """Decrypt an encrypted data key"""
 
@@ -70,29 +79,13 @@ def decrypt_data_key(data_key_encrypted):
 
     # Return plaintext base64-encoded binary data key
     return base64.b64encode((response["Plaintext"]))
-    
-def encrypt_file(file, key):
-    """Encrypt JSON data using an AWS KMS CMK"""
 
-    # Encrypt the data
-    f = Fernet(key)
-    file_encrypted = f.encrypt(file)
-
-    return file_encrypted
-    
-def decrypt_file(file, key):
-    """Decrypt a file encrypted by encrypt_file()"""
-
-    # Decrypt the rest of the file
-    f = Fernet(key)
-    file_decrypted = f.decrypt(file)
-
-    return file_decrypted
-
+# Main web page (sign in)
 @app.route('/')
 def home():
     return render_template('signin.html')
 
+# Display files
 @app.route('/display', methods = ['GET', 'POST'])
 def sign_in():
     if request.method == 'POST':
@@ -100,6 +93,7 @@ def sign_in():
         password = request.form.get('password')
         print('name: ' + name + ' -> password: ' + password)
         try:
+            # Check if password is correct and list files
             if user_password[name] == password:
                 files = os.listdir(os.path.join(path, name))
                 return render_template('files.html', files=files, name=name, password=password)
@@ -108,6 +102,7 @@ def sign_in():
         except KeyError:
             return redirect('/signup')
 
+# Sign up
 @app.route('/signup', methods = ['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -116,24 +111,38 @@ def sign_up():
         mode = request.form.get('mode')
         print('name: ' + name + ' -> password: ' + password + ' -> mode: ' + mode)
         try:
+            # Sign up user if it doesn't exist
+            try:
+                # If user exists, redirect to sign up again
+                user_password[name]
+                print(name + ' already exists')
+                return redirect('/signup')
+            except:
+                print(name + ' created')
+                pass
+            # Check crypto algorithm and save it (name: mode)
             if (mode == 'fernet' or mode == 'aead'):
                 user_mode[name] = mode
             else:
                 return redirect('/signup')
+            # Save password (name: password)
             user_password[name] = password
-            key = base64.urlsafe_b64encode(os.urandom(32))
-            #key = create_data_key(create_cmk(name))
-            user_key[name] = key
+            # Create AWS key
+            temp = create_cmk(name)
+            # Create directory if it doesn't exist
             try:
                 os.mkdir(os.path.join(path, name))
             except:
                 pass
+            # If it's OK, redirect to main page (sign in)
             return redirect('/')
         except:
+            # If something went wrong, redirect to sign up again
             return redirect('/signup')
     else:
         return render_template('signup.html')
 	
+# Upload file
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -141,19 +150,26 @@ def upload_file():
         password = request.form.get('password')
         f = request.files['file']
         try:
+            # Check if password is correct, upload file and save it encrypted
             if user_password[name] == password:
-                key = user_key[name]
+                key_enc, key = create_data_key(retrieve_cmk(name))
+                # Encrypt using Fernet
                 if user_mode[name] == 'fernet':
                     fernet = Fernet(key)
                     file_enc = fernet.encrypt(f.read())
+                # Encrypt using AEAD
                 else:
                     derived_key_aead = HKDF(algorithm = hashes.SHA256(), length = 24, salt = None, info = None).derive(key)
                     key_aead = base64.urlsafe_b64encode(derived_key_aead)
                     aesgcm = aead.AESGCM(key_aead)
                     file_enc = aesgcm.encrypt(b"12345678", f.read(), None)
+                # Save file in path
                 with open(os.path.join(path + name + '/', secure_filename(f.filename)), 'wb') as file:
-                    file.write(file_enc) 
-                # f.save(os.path.join(path + name + '/', secure_filename(f.filename)))
+                    # Save encrypted key on file
+                    file.write(len(key_enc).to_bytes(NUM_BYTES_FOR_LEN, byteorder='big'))
+                    file.write(key_enc)
+                    file.write(file_enc)
+                # List files again
                 files = os.listdir(os.path.join(path, name))
                 return render_template('files.html', files=files, name=name, password=password)
             else:
@@ -161,41 +177,52 @@ def upload_file():
         except KeyError:
             return redirect('/signup')
 
+# Download file
 @app.route('/download/<filename>', methods = ['GET', 'POST'])
 def upload(filename):
     if request.method == 'POST':
         name = request.form.get('name')
         password = request.form.get('password')
         try:
+            # Check if password is correct, decrypt file and download
             if user_password[name] == password:
                 with open(os.path.join(path + name + '/', filename), "rb") as file:
                     file_enc = file.read()
-                key = user_key[name]
+                # Read key from file and decrypt it
+                key_enc_len = int.from_bytes(file_enc[:NUM_BYTES_FOR_LEN], byteorder="big") + NUM_BYTES_FOR_LEN
+                key_enc = file_enc[NUM_BYTES_FOR_LEN:key_enc_len]
+                key = decrypt_data_key(key_enc)
                 if user_mode[name] == 'fernet':
+                    # Encrypt using Fernet
                     fernet = Fernet(key)
-                    file_dec = fernet.decrypt(file_enc)
+                    file_dec = fernet.decrypt(file_enc[key_enc_len:])
                 else:
+                    # Encrypt using AEAD
                     derived_key_aead = HKDF(algorithm = hashes.SHA256(), length = 24, salt = None, info = None).derive(key)
                     key_aead = base64.urlsafe_b64encode(derived_key_aead)
                     aesgcm = aead.AESGCM(key_aead)
-                    file_dec = aesgcm.decrypt(b"12345678", file_enc, None)
+                    file_dec = aesgcm.decrypt(b"12345678", file_enc[key_enc_len:], None)
+                # Send file
                 file = BytesIO(file_dec)
                 return send_file(file, attachment_filename=filename)
-                #return send_from_directory(os.path.join(path, name), filename)
             else:
                 return redirect('/')
         except KeyError:
             return redirect('/signup')
 
+# Delete file
 @app.route('/delete/<filename>', methods = ['GET', 'POST'])
 def delete(filename):
     if request.method == 'POST':
         name = request.form.get('name')
         password = request.form.get('password')
         try:
+            # Check if password is correct and delete file
             if user_password[name] == password:
+                # Secure delete
                 secure_delete.secure_random_seed_init()
                 secure_delete.secure_delete(os.path.join(path + name + '/', filename))
+                # List files again
                 files = os.listdir(os.path.join(path, name))
                 return render_template('files.html', files=files, name=name, password=password)
             else:
